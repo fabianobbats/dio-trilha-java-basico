e87ccd564dfe6449e36345132963a256726cf3e2
Fabiano Batista <fabianobatista@outlook.com>
2024-09-20 21:13:42 -0400

    criados as funções para ler e mostrar os dados ao usuario

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner((System.in));
+
+        // Solicita e lê o número da agência
+        System.out.println("Por favor, digite o número da Agência:");
+        String numeroAgencia = scanner.nextLine();
+
+        // Solicita e lê o número da conta
+        System.out.println("Por favor, digite o número da Conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        // Solicita e lê o nome do cliente
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        // Solicita e lê o saldo da conta
+        System.out.println("Por favor, digite o saldo da Conta:");
+        double saldoConta = scanner.nextDouble();
+
+        // Exibe a mensagem final
+        String mensagem = "Olá " + nomeCliente
+                + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia
+                + ", conta " + numeroConta + " e seu saldo " + saldoConta
+                + " já está disponível para saque.";
+        System.out.println(mensagem);
+
+        scanner.close();
+
     }
 }
